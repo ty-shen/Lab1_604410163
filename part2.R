83284b37f318917489f4071f73cf6f4b08194e23
ty-shen <tyshen@mail.fcu.edu.tw>
2015-09-24 12:18:00 +0800

    Lab1_604410163.rar

---
+++ part2.R
@@ -0,0 +1,4 @@
+sum <- 0
+for(i in 1:100)
+{sum = sum+i*i}
+print(sum)
---
+++ part3.R
@@ -0,0 +1,24 @@
+Wingcrd <- c(59, 55, 53.5, 55, 52.5, 57.5, 53, 55)
+Tarsus <- c(22.3, 19.7, 20.8, 20.3, 20.8, 21.5, 20.6, 21.5)
+Head <- c(31.2, 30.4, 30.6, 30.3, 30.3, 30.8, 32.5, NA)
+Wt <- c(9.5, 13.8, 14.8, 15.2, 15.5, 15.6, 15.6, 15.7)
+
+mean(Wingcrd)
+median(Wingcrd)
+min(Wingcrd)
+max(Wingcrd)
+
+mean(Tarsus)
+median(Tarsus)
+min(Tarsus)
+max(Tarsus)
+
+mean(Head)
+median(Head)
+min(Head)
+max(Head)
+
+mean(Wt)
+median(Wt)
+min(Wt)
+max(Wt)
